46b7d9493da4adf8b244a6f7a9480cf77dbcdf77
kimdonguk <slasnrndu@gmail.com>
2021-05-11 13:48:12 +0900

    todo refactoring

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <todo-header></todo-header>
-    <todo-input></todo-input>
-    <todo-list></todo-list>
-    <todo-footer></todo-footer>
+    <todo-input v-on:addTodoItem="addOneItem"></todo-input>
+    <todo-list v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></todo-list>
+    <todo-footer v-on:clearItem="clearAllItem"></todo-footer>
   </div>
 </template>
 
@@ -14,6 +14,46 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    addOneItem: function(todoItem) {
+      // 저장하는 로직
+      var obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem,JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+    removeOneItem: function(todoItem, index) {
+      console.log(todoItem);
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+    toggleOneItem: function(todoItem, index) {
+      // todoItem.completed = !todoItem.completed;
+      this.todoItems[index].completed = !this.todoItems[index].completed
+      // 로컬스토리지에 데이터 저장
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItem: function() {
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i++) {
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+         // this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  },
   components: {
     'todo-header':TodoHeader,
     'todo-input':TodoInput,
@@ -24,5 +64,18 @@ export default {
 </script>
 
 <style>
-
+  body {
+    text-align: center;
+    background-color: #F6F6F6;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
+  }
 </style>
